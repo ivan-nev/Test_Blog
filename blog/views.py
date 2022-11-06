f19f6c2253b8b066721789e92bdce051f1533360
ivan-nev <i_nevakshenov@mail.ru>
2022-11-06 22:28:14 +0500

    2 commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(publisher_data__lte=timezone.now()).order_by('publisher_data')
+    context = {'posts':posts}
+    return render(request, 'blog/post_list.html', context=context)
 
 # Create your views here.
